820eff7d97522a51c7ccd7072b024ec0f7b93b98
SP <SP@007.com>
2022-03-17 15:20:21 -0700

    find missing Number in Array

--- src/Basics.java
+++ src/Basics.java
@@ -9,28 +9,26 @@ public class Basics {
 	public static void main(String[] args)
 	{
     // Arrays are equal or not without using Arrays.equals
-		
-		
-		int[] a = {1,2,3} ;
-		int [] b = {1,2,3} ;
-		int [] c = {4,5,6,7} ;
-		boolean status = true;
-		
-		if(a.length== b.length)
-		{
-			for(int i =0 ;i<a.length-1;i++)
-			{
-				if(a[i]!=b[i])
-					status = false;
-			}
-			
-			
-		}
-		else
-		{
-			status = false ;
-		}
-		System.out.println("Status is " +status);
-	
+		// calculate sum of array and calculate sum of i elements
+		// find the difference between array
+    int sum1  =  0;
+    int sum2 = 0 ;
+    int[] a= {1,2,4};
+    for(int i =0 ;i<=a.length-1;i++)
+    {
+    	sum1 =sum1 +a[i];
+    }
+    
+    System.out.println("a[i]"+a[a.length-1]);
+    // use i to add sum2 
+    for(int i =0 ;i<=a[a.length-1];i++)
+    {  //System.out.println("Sum value each Iteration"+sum2);
+    	sum2 =sum2 +i;
+    }
+    
+    
+    System.out.println(" Sum 2 is" +sum2);
+	// use brackets for sum2-sum1
+    System.out.println("Missing Numbee is " + (sum2-sum1));
 	}
 	}
