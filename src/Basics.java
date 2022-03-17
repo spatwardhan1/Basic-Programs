a4fae08440cb59e5347c1cd1b61087a088e78fe5
SP <SP@007.com>
2022-03-16 18:58:41 -0700

    even and odd count

--- src/Basics.java
+++ src/Basics.java
@@ -5,15 +5,23 @@ public class Basics {
 	{
 		// count the numbers
 		// untill num>0 divide each number 
-    int num = 1234 ;
-    int count  = 0 ;
+    int num = 12345 ;
+    int even_count  = 0 ;
+    int odd_count  = 0 ;
+    
     while(num>0)
-    {
+    {   int rem = num%10 ;
+    
+      if(rem%2==0)
+    	  even_count++ ;
+      else
+    	  odd_count++ ;
     	num =num/10 ;
-    	count++ ;
+    	
     }
     
-    System.out.println("Return count" +count);
+    System.out.println("Return even count" +even_count);
+    System.out.println("return odd count" +odd_count);
 		
 }
 	
