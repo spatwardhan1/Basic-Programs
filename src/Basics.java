08cac97f8fa0c595d51ceb24d5e1003ee124c472
SP <SP@007.com>
2022-03-16 18:55:39 -0700

    count the number of digits in Number

--- src/Basics.java
+++ src/Basics.java
@@ -3,37 +3,17 @@ public class Basics {
 	
 	public static void main(String[] args)
 	{
-		
-    // check palindrome string
-		
-		// reverse the string
-		// check the reverse string with original string
-		
-		String str = "MADAM" ;
-		String abc = "MADAM";
-		String original = str ;
-		String rev = "";
-		
-		
-		for(int i = str.length()-1;i>=0;i--)
-		{
-			rev = rev + str.charAt(i);
-			
-		}
-		
-		System.out.println("reverse " +rev);
-		System.out.println("Original" +original);
-		System.out.println(original.equalsIgnoreCase(rev));
-		// if getting false "" should not have space
-		if(original.equalsIgnoreCase(rev))
-		{
-			System.out.println(" Palidrome in String");
-		}
-		
-		else
-		{
-			System.out.println(" Not a palindrome");
-		}
+		// count the numbers
+		// untill num>0 divide each number 
+    int num = 1234 ;
+    int count  = 0 ;
+    while(num>0)
+    {
+    	num =num/10 ;
+    	count++ ;
+    }
+    
+    System.out.println("Return count" +count);
 		
 }
 	
