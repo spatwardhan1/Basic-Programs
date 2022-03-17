c7d93b7ce5e5ce319176d6506f8ba7e9b31a8a77
SP <SP@007.com>
2022-03-16 19:06:00 -0700

    add number of sum

--- src/Basics.java
+++ src/Basics.java
@@ -4,24 +4,21 @@ public class Basics {
 	public static void main(String[] args)
 	{
 		// count the numbers
-		// untill num>0 divide each number 
-    int num = 12345 ;
-    int even_count  = 0 ;
-    int odd_count  = 0 ;
-    
-    while(num>0)
-    {   int rem = num%10 ;
-    
-      if(rem%2==0)
-    	  even_count++ ;
-      else
-    	  odd_count++ ;
-    	num =num/10 ;
+		// create sum
+		// sum + each number num%10
+     int num = 12345 ;
+     int sum = 0 ;
+     
+     while(num > 0)
+     {
+    	 
+    	 sum = sum + num%10 ;
+    	 num = num/10 ;
     	
     }
     
-    System.out.println("Return even count" +even_count);
-    System.out.println("return odd count" +odd_count);
+    System.out.println("Return sum" +sum);
+    //System.out.println("return odd count" +odd_count);
 		
 }
 	
