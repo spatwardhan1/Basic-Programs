328c1875330d2e05871fa1f31e800e33aae9495f
SP <SP@007.com>
2022-03-17 03:17:08 -0700

    fibonacci series

--- src/Basics.java
+++ src/Basics.java
@@ -3,18 +3,22 @@ public class Basics {
 	
 	public static void main(String[] args)
 	{
-		// greatest number of 3 characters
-		// check between a and b and then c and combination of a and b
-		 int a = 2 ;
-		 int b = 5 ;
-		 int c = 2 ;
-		int largest = a>b?a:b ;
-		int largest2  = c >largest?c:largest ;
+	  // fibonacci
 		
-		System.out.println("Largest" + largest2);
-		
+		int n1 = 0 ;
+		int n2 = 1;
+		int sum  = 0 ;
      
 		
+		for(int i=2;i<10;i++)
+		{
+			sum = n1 + n2 ;
+			System.out.println("Fibonacci series" +sum);
+			n1 = n2 ;
+			n2  = sum ;
+		}
+		
+		System.out.println("Total FIbonacci series is"+sum);
 }
 	
 }
