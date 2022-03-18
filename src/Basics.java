d5c302b401c6a4516c6156e2f6448672d41e88f3
SP <SP@007.com>
2022-03-18 16:12:47 -0700

    Read Text File

--- src/Basics.java
+++ src/Basics.java
@@ -1,31 +1,32 @@
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Random;
+import java.util.Scanner;
 
 import org.apache.commons.lang3.RandomStringUtils;
 
+import java.io.File;
 import java.lang.Object;
 
 public class Basics {
 
 	public static void main(String[] args)
 	{
-  // reverse each word from String
-	  String s = "Welcome to JAVA";
-	  String[] words = s.split("\\s");
-	  String rev = "" ;
-	  // take each word
-	  for(String w:words)
-	  {
-		  StringBuilder b = new StringBuilder(w);
-		  // reverse each word
-		  b.reverse();
-		  // store in rev and after each word put space
-		  rev =rev + b.toString() + " ";
-	  }
-	  
-	  System.out.println("Reverse String is" +rev);
-	   }
+   // Read from FIle
+		File file = new File("C:\\Users\\SP\\Downloads\\Test.txt");
+	
+		try {
+		Scanner scan = new Scanner(file);
+		// Z will match every line
+		scan.useDelimiter("\\Z");
+		System.out.println(scan.next());
+		}catch(Exception e)
+		{
+			System.out.println("FIle Incorrect path");
+			
+		}
+
+	}
 	}
 	
 	
