8b041d83731c27852e6b5bfd1beeeafb36cce5e9
SP <SP@007.com>
2022-03-18 14:04:18 -0700

    min and max in array

--- src/Basics.java
+++ src/Basics.java
@@ -8,27 +8,36 @@ public class Basics {
 
 	public static void main(String[] args)
 	{
-    // Arrays are equal or not without using Arrays.equals
-		// calculate sum of array and calculate sum of i elements
-		// find the difference between array
-    int sum1  =  0;
-    int sum2 = 0 ;
-    int[] a= {1,2,4};
-    for(int i =0 ;i<=a.length-1;i++)
-    {
-    	sum1 =sum1 +a[i];
-    }
-    
-    System.out.println("a[i]"+a[a.length-1]);
-    // use i to add sum2 
-    for(int i =0 ;i<=a[a.length-1];i++)
-    {  //System.out.println("Sum value each Iteration"+sum2);
-    	sum2 =sum2 +i;
-    }
-    
-    
-    System.out.println(" Sum 2 is" +sum2);
-	// use brackets for sum2-sum1
-    System.out.println("Missing Numbee is " + (sum2-sum1));
+   // find maximum element in array
+		
+		// a[i]> max
+		
+		
+		int a[] = {10,20,30,40};
+		
+		int max = 0 ;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]>max)
+				max = a[i];
+		}
+		
+		System.out.println(" max value " + max);
+		
+		int min = a[0];
+		
+		for(int i=0;i<=a.length-1;i++)
+		{
+			if(a[i]<min)
+				min = a[i];
+		}
+		
+		System.out.println("Minimum element" + min);
 	}
+	
+	
+	
+	
+	
+	
 	}
