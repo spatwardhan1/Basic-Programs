454cc1cfb5cf3ce47fa682690b58c622532cd355
SP <SP@007.com>
2022-03-20 16:59:17 -0700

    create stream and use filter

--- src/Basics.java
+++ src/Basics.java
@@ -4,6 +4,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -23,22 +25,17 @@ public class Basics {
 	public static void main(String[] args)
 	{
 		
-	List<Integer>  list = new ArrayList<>() ;
-	List<Integer>  list3 = Arrays.asList(23, 22,10,5);
-	for(int i: list3)
-	{
-		if(i%2 == 0)
-		{
-			list.add(i);
-			System.out.println("  Is Even Number");
-		}
-		else 
-		{
-			System.out.println(" Not A Even ");
-		}
-	}
-	
-	System.out.println("list 1" + list);
+		List<Integer>  list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		System.out.println("New List"+list);
+	   // create stream
+		Stream<Integer> stream = list.stream();
+		// preform operation on stream
+		// filter to add boolean condition
+		// and finally collected the filtered elements into List
+		List<Integer> newList = stream.filter(i->i%2==0).collect(Collectors.toList());
+		System.out.println(" List afete stream" +newList);
 	
 	}
 	}
