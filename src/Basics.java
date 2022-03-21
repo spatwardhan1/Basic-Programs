1fd1e1192c46eb977bff455193dde7e331b3fd29
SP <SP@007.com>
2022-03-20 21:29:44 -0700

    Iterator and List Iterator

--- src/Basics.java
+++ src/Basics.java
@@ -1,8 +1,10 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.TreeSet;
@@ -28,60 +30,29 @@ public class Basics {
 	public static void main(String[] args)
 	{
 		
-   // type safe ->  same type of elements
-		
-		ArrayList<String>  str = new ArrayList<>();
-		
-		str.add("Soham");
-		str.add("Patwardhan");
-		str.add("Sachin");
-		System.out.println(str);
-		// get from zero th Index
-		System.out.println(str.get(0));
-		
-		// remove Sachin 
-		str.remove("Sachin");
-		// get size after removing Sachin
-		System.out.println("Size" +str.size());
-		
-		// Check if Sachin Present
-		System.out.println("Checking Sachin present" +str.contains("Sachin"));
-		
-		// check if EMpty 
-		
-		System.out.println("CHecking empty or Not" +str.isEmpty());
-		
-		// setting value for 1st element
-		str.set(1, "SP");
-		
-		
-	
-		System.out.println("str by setting" +str);
-		// add will push the current  elemetn  to next and add at the iven index
-		str.add(1, "SP NEW");
-		System.out.println(str);
-		// clear to remove all elements
-	;
-		
-		// un type safe -> not a same type of elements
-		
-	   LinkedList  list = new LinkedList();
-	   // can have any type of vars
-	   list.add("Sachin");
-	   list.add(1.00);
-	   list.add(true);
-	   System.out.println(list);
-		
-	   
-	   
-	   
-	  System.out.println("_____________________________");
-	  
-	  TreeSet  tree = new TreeSet();
-	  System.out.println("Tree Set Values");
-	  tree.add(14.14);
-	  tree.add(13.12);
-	  System.out.println(tree);
+    ArrayList<String>  list = new ArrayList<>();
+    list.add("Soham");
+    list.add("Patwardhan");
+    list.add("ABCD");
+    
+    // call the Iterator method and pass to Object
+    
+    Iterator<String>  itr = list.iterator();
+    
+    System.out.println("Iterator calling");
+    while(itr.hasNext())
+    {
+    	System.out.println(itr.next());
+    }
+    
+    // backward Traversal using List Iterator and give size so the last ele is pointed
+    System.out.println("Traversing in Reverse Order");
+   ListIterator<String>  litr = list.listIterator(list.size());
+   
+   while(litr.hasPrevious())
+   {
+	   System.out.println(litr.previous());
+   }
 	  
 	 
 		
